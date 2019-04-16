e4027a0bcbdcafc457a7a47cd6a866aa5c727231
AgusQuintanar <agusquintanar17@gmail.com>
2019-04-16 00:44:54 -0500

    Threads simplification (Reduction to only one)

--- PacMan/PacMan.java
+++ PacMan/PacMan.java
@@ -1,39 +1,22 @@
-import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import java.awt.geom.AffineTransform;
 import java.awt.image.ImageObserver;
-
-import javax.swing.Icon;
 import javax.swing.ImageIcon;
-import javax.swing.JPanel;
 
-public class PacMan extends JPanel implements ImageObserver, Runnable {
+public class PacMan implements ImageObserver {
 	protected int xPac,
 				  yPac,
 				  ancho;
 	private Image pacManAbierto,
 				  pacManCerrado;
-	private Thread hilo;
-	private boolean abiertoCerrado;
-	
+				  
 	public PacMan(int xPac, int yPac) {
-		super();
 		this.xPac = xPac;
 		this.yPac = yPac;
 		this.ancho = 40;
-		this.setPreferredSize(new Dimension(this.ancho,this.ancho));
-		this.setBackground(Color.BLACK);
-		this.hilo = new Thread(this);
-		this.hilo.start();
+		this.pacManAbierto = new ImageIcon("Imagenes/pac-man-3d-arr.png").getImage();
+		this.pacManCerrado = new ImageIcon("Imagenes/PacManIconCerrado-3d-arr.png").getImage();
 	}
-
-	public void paintComponent(Graphics g, String direccionPacman) {
-		super.paintComponent(g);
-		this.pintaPacman(g,this.abiertoCerrado, direccionPacman);
-    }
 
 	public void pintaPacman(Graphics g, boolean abiertoCerrado, String direccionPacman) {
 		this.pacManAbierto = new ImageIcon("Imagenes/pac-man-3d-"+direccionPacman+".png").getImage();
@@ -46,22 +29,6 @@ public class PacMan extends JPanel implements ImageObserver, Runnable {
 			g.drawImage(this.pacManCerrado, xPac, yPac, this.ancho, this.ancho, this);
 		}
 	}
-
-	public void run(){
-		while(true){
-			this.repaint();
-			this.abiertoCerrado = true;
-			try {
-				Thread.sleep(150);
-				this.abiertoCerrado = false;
-				this.repaint();
-				Thread.sleep(150);
-		}catch(InterruptedException ex) {
-			System.out.println("No pude despertar!");
-			}
-		}
-	}
-	
 
 	@Override
 	public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
--- PacMan/Pista.java
+++ PacMan/Pista.java
@@ -3,16 +3,14 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import javax.swing.JPanel;
 
 public class Pista extends JPanel implements Runnable, KeyListener {
 	private PacMan pacman;
-    private Thread hilo;
-		private String direccionPacman,
+  private Thread hilo;
+	private String direccionPacman,
 									 direccionTmp = "arr";
-
+	private boolean abiertoCerrado;
 
 	public Pista() {
 		super();
@@ -20,8 +18,8 @@ public class Pista extends JPanel implements Runnable, KeyListener {
 		this.setPreferredSize(new Dimension(anchoPista,anchoPista));
 		this.setBackground(Color.BLACK);
 		this.pacman = new PacMan(anchoPista/2-anchoPista/34, anchoPista/2-anchoPista/34);
-		this.pacman.setLocation(this.pacman.xPac, this.pacman.yPac);
 		this.direccionPacman = "";
+		this.abiertoCerrado = true;
 		this.direccionTmp = "arr";
 		this.setFocusable(true);
 		this.addKeyListener(this);
@@ -31,21 +29,33 @@ public class Pista extends JPanel implements Runnable, KeyListener {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		this.pacman.paintComponent(g, this.direccionTmp);
+		this.pacman.pintaPacman(g,this.abiertoCerrado, this.direccionTmp);
     }
     
 	public void run(){
 		int velocidad = 4;
+		int contMiliSeg = 0;
 		while(true){
 			if(direccionPacman != "") //Si esta parado se guarda su posicion y direccion
 				direccionTmp = direccionPacman;
+			this.repaint();
 			try {
-				Thread.sleep(5);
+				Thread.sleep(3);
+				contMiliSeg += 3;
+				System.out.println("Cont 1: "+contMiliSeg);
+				if(contMiliSeg % 147 == 0) this.abiertoCerrado = false;
 				if(this.direccionPacman == "der") this.pacman.xPac += velocidad;
 				else if(this.direccionPacman == "izq") this.pacman.xPac -= velocidad;
 				else if(this.direccionPacman == "arr") this.pacman.yPac -= velocidad;
 				else if(this.direccionPacman == "aba") this.pacman.yPac += velocidad;
-				this.pacman.setLocation(this.pacman.xPac, this.pacman.yPac);
+				Thread.sleep(3);
+				contMiliSeg += 3;
+				System.out.println("Cont 2: "+contMiliSeg);
+				if(contMiliSeg % 300 == 0){
+					this.abiertoCerrado = true;
+					contMiliSeg = 0; //Reinicia el contador
+				}
+				
 				this.repaint();
 
 		}catch(InterruptedException ex) {
