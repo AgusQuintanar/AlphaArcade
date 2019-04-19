f051cd8d5eb80085f32060b43b6692a94b91c079
AgusQuintanar <agusquintanar17@gmail.com>
2019-04-18 21:25:37 -0500

    Improve movement

--- PacMan/Pista.java
+++ PacMan/Pista.java
@@ -4,170 +4,268 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
-public class Pista extends JPanel implements Runnable, KeyListener {
+public class Pista extends JPanel implements Runnable, KeyListener, MouseListener {
 	private PacMan pacman;
-  private Thread hilo;
-	private String direccionPacman,
-									 direccionTmp = "arr";
-	private boolean abiertoCerrado;
+	private Thread hilo;
+	private String direccionPacman, direccionTmp = "arr";
+	private boolean abiertoCerrado,
+									sentidoX;
 	private Image pista;
-	private double ancho,
-					alto;
+	private double ancho, alto;
 	private int[][] matrizPista;
-	private int contador;	
-	
+	private int contador;
+
 	public Pista(double ancho) {
 		super();
-		int anchoOriginal = 1749,
-				altoOriginal = 1047;
-		double	proporcionAncho = ancho/anchoOriginal; //Considerando un ancho del 100%
-		System.out.println(ancho+" "+alto);
-		this.ancho = (int)(anchoOriginal*proporcionAncho);
-		this.alto = (int)(altoOriginal*proporcionAncho);
+		int anchoOriginal = 1749, altoOriginal = 1047;
+		double proporcionAncho = ancho / anchoOriginal; // Considerando un ancho del 100%
+		System.out.println(ancho + " " + alto);
+		this.ancho = (int) (anchoOriginal * proporcionAncho);
+		this.alto = (int) (altoOriginal * proporcionAncho);
 		System.out.println(proporcionAncho);
-		System.out.println(("ancho: "+this.ancho)+" alto: "+this.alto);
-		this.setPreferredSize(new Dimension((int)this.ancho,(int)this.alto));
+		System.out.println(("ancho: " + this.ancho) + " alto: " + this.alto);
+		this.setPreferredSize(new Dimension((int) this.ancho, (int) this.alto));
 		this.setBackground(Color.BLACK);
-		this.pacman = new PacMan((int)(this.ancho/2-this.ancho/104), (int)(.95*this.alto/2-.95*this.alto/62), (int)((this.ancho/52)*1.35));
+		this.pacman = new PacMan((int) (this.ancho / 2 - this.ancho / 104),
+				(int) (.95 * this.alto / 2 - .95 * this.alto / 62), (int) ((this.ancho / 52) * 1.25));
 		this.direccionPacman = "";
 		this.abiertoCerrado = true;
 		this.direccionTmp = "arr";
+		this.sentidoX = true; //Izquierda es false y derecha true
 		this.pista = new ImageIcon("Imagenes/PistaConPuntitos.png").getImage();
 		this.setFocusable(true);
 		this.addKeyListener(this);
 		this.contador = 0;
 		this.hilo = new Thread(this);
 		this.hilo.start();
+		this.addMouseListener(this);
 		this.matrizPista = new int[][] {
-			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-			{1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-			{1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
-			{1,0,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,0,1},
-			{1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,3,1,1,3,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,0,0,0,0,0,0,0,1,1,3,3,3,3,3,3,3,3,3,3,1,1,0,0,0,0,0,0,0,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1,3,1,1,1,3,3,1,1,1,3,1,1,0,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,0,1,1,3,1,3,3,3,3,3,3,1,3,1,1,0,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1},
-			{2,2,2,2,2,2,0,0,0,0,0,0,0,1,1,0,0,0,0,3,3,3,1,3,3,3,3,3,3,1,3,3,3,0,0,0,0,1,1,0,0,0,0,0,0,0,2,2,2,2,2,2},
-			{1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,3,1,3,3,3,3,3,3,1,3,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,3,1,1,1,1,1,1,1,1,3,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,0,0,0,0,0,0,0,0,0,1,1,3,3,3,3,3,3,3,3,3,3,1,1,0,0,0,0,0,0,0,0,0,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,3,1,1,1,1,1,1,1,1,3,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,3,1,1,1,1,1,1,1,1,3,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,3,0,0,0,1,1,0,0,0,3,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-			{1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1},
-			{1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,1},
-			{1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
-			{1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1},
-			{1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1},
-			{1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
-			{1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1},
-			{1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1},
-			{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
-		};
-		
+				{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+						1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+						1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0, 0,
+						0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 3, 3, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1,
+						1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 3, 3, 3, 3, 3, 3, 1, 3, 1, 1, 0, 1, 1, 1,
+						1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 3, 3, 3, 1, 3, 3, 3, 3, 3, 3, 1, 3, 3, 3, 0, 0, 0, 0,
+						1, 1, 0, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 3, 1, 3, 3, 3, 3, 3, 3, 1, 3, 1, 1, 1, 1, 1, 0,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 1, 1, 1, 0,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 3, 1, 1, 0, 1, 1, 1,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 1, 1, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0,
+						1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1 },
+				{ 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1 },
+				{ 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1,
+						1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
+						1, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+						1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1 },
+				{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+						0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1 },
+				{ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+						1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 } };
+
 	}
 
-	public int getAlto(){
-		return (int)this.alto;
+	public int getAlto() {
+		return (int) this.alto;
 	}
 
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(this.pista, 0, 0, (int)this.ancho, (int)this.alto, this);
-		this.pacman.pintaPacman(g,this.abiertoCerrado, this.direccionTmp);
-    }
-    
-	public void run(){
+		g.drawImage(this.pista, 0, 0, (int) this.ancho, (int) this.alto, this);
+		this.pacman.pintaPacman(g, this.abiertoCerrado, this.direccionTmp);
+	}
+
+	public void run() {
 		long lastTime = System.nanoTime();
 		final double amountOfTicks = 60.0;
-		double ns = 1000000000 / amountOfTicks,
-					 delta = 0.0;
+		double ns = 1000000000 / amountOfTicks, delta = 0.0;
 		int fps = 0;
-		
-		long timer = System.currentTimeMillis();					 
-		
-		while(true){
+
+		long timer = System.currentTimeMillis();
+
+		while (true) {
 			long now = System.nanoTime();
 			delta += (now - lastTime) / ns;
 			lastTime = now;
-			while (delta >= 1){
+			while (delta >= 1) {
 				tick();
 				render();
 				this.contador++;
 				fps++;
 				delta--;
-	
-			}
-			
 
-			if(System.currentTimeMillis() - timer > 1000){
+			}
+
+			if (System.currentTimeMillis() - timer > 1000) {
 				timer += 1000;
-				System.out.println("Fps " + fps);
+				// System.out.println("Fps " + fps);
 				fps = 0;
 			}
 
 		}
 	}
-			
 
-	private void tick (){
+	private void tick() {
 
 		int velocidad = 8;
-		
-		int coorX = 0,
-				coorY = 0;
 
-		coorX = (int) (((this.pacman.xPac)*52)/this.ancho); 
-			coorY = (int) (((this.pacman.yPac*31)/this.alto)/.975) + (int)((((this.alto*.95)/62)*31)/this.alto);
-			if(direccionPacman != "") //Si esta parado se guarda su posicion y direccion
-				direccionTmp = direccionPacman;
-	
-			if(this.contador % 17 == 0) this.abiertoCerrado = false;
-			if(this.direccionPacman == "der" && this.matrizPista[coorY][coorX+1] != 1) this.pacman.xPac += velocidad;
-			else if(this.direccionPacman == "izq" && this.matrizPista[coorY][coorX-1] != 1) this.pacman.xPac -= velocidad;
-			else if(this.direccionPacman == "arr" && this.matrizPista[coorY-1][coorX] != 1) this.pacman.yPac -= velocidad;
-			else if(this.direccionPacman == "aba" && this.matrizPista[coorY+1][coorX] != 1) this.pacman.yPac += velocidad;
-			//System.out.println("CooRealesP: "+this.pacman.xPac+", "+this.pacman.yPac);
-			//System.out.println("Coordenadas: "+ coorX + ", " + coorY);
+		int coorX = 24,
+		 coorY = 0;
 
-			if(this.contador% 33 == 0){
-				this.abiertoCerrado = true;
-				this.contador = 1; //Reinicia el contador a 1 (Para no dibujar la imagen de la pista de nuevo)
-			}
+		boolean subirBajar = true; //Dtermina si pacman puede subir o bajar dependiendo del margen de error
+		double coorXTemp = ((this.pacman.xPac + this.ancho/104 + this.ancho/15600)*52) / (this.ancho*1.00015) + .2;
+
+		System.out.println(coorXTemp);
+
+		if (Math.abs((int)coorXTemp - coorXTemp) < 0.20){
+			coorX = (int)coorXTemp;
+			System.out.println("Caso 1");
+		}
+		else if (Math.abs((int)coorXTemp+1 - coorXTemp) < 0.20){
+			coorX = (int)coorXTemp+1;
+			System.out.println("Caso 2");
+		}  
+		else if (sentidoX || coorXTemp > 50.0) {
+			subirBajar = false;
+			coorX = (int)coorXTemp;
+			System.out.println("Caso 3");
+		}
+		else{
+			subirBajar = false;
+			coorX = (int)coorXTemp + 1;
+			System.out.println("Caso 4");
+		}
+
+		System.out.println(coorX);
+		coorY = (int) (((this.pacman.yPac * 31) / this.alto) / .975) + (int) ((((this.alto * .95) / 62) * 31) / this.alto);
+		if (direccionPacman != "") // Si esta parado se guarda su posicion y direccion
+			direccionTmp = direccionPacman;
+
+		if (this.contador % 17 == 0)
+			this.abiertoCerrado = false;
+		if (this.direccionPacman == "der" && this.matrizPista[coorY][coorX + 1] != 1){
+			this.pacman.xPac += velocidad;
+			this.sentidoX = true;
+		}	
+		else if (this.direccionPacman == "izq" && this.matrizPista[coorY][coorX - 1] != 1){
+			this.pacman.xPac -= velocidad;
+			this.sentidoX = false;
+		}
+		else if (this.direccionPacman == "arr" && this.matrizPista[coorY - 1][coorX] != 1 && subirBajar)
+			this.pacman.yPac -= velocidad;
+		else if (this.direccionPacman == "aba" && this.matrizPista[coorY + 1][coorX] != 1 && subirBajar)
+			this.pacman.yPac += velocidad;
+		//System.out.println("CooRealesP: " + this.pacman.xPac + ", " + this.pacman.yPac);
+		//System.out.println("Coordenadas: " + coorX + ", " + coorY);
+
+		if (this.contador % 33 == 0) {
+			this.abiertoCerrado = true;
+			this.contador = 1; // Reinicia el contador a 1 (Para no dibujar la imagen de la pista de nuevo)
+		}
 	}
 
-	private void render(){
+	private void render() {
 		this.repaint();
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) this.direccionPacman = "der";
-		else if(e.getKeyCode() == KeyEvent.VK_LEFT) this.direccionPacman = "izq";
-		else if(e.getKeyCode() == KeyEvent.VK_UP) this.direccionPacman = "arr";
-		else if(e.getKeyCode() == KeyEvent.VK_DOWN) this.direccionPacman = "aba";
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			this.direccionPacman = "der";
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			this.direccionPacman = "izq";
+		else if (e.getKeyCode() == KeyEvent.VK_UP)
+			this.direccionPacman = "arr";
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			this.direccionPacman = "aba";
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode() == KeyEvent.VK_RIGHT) this.direccionPacman = "";
-		else if(e.getKeyCode() == KeyEvent.VK_LEFT) this.direccionPacman = "";
-		else if(e.getKeyCode() == KeyEvent.VK_UP) this.direccionPacman = "";
-		else if(e.getKeyCode() == KeyEvent.VK_DOWN) this.direccionPacman = "";
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+			this.direccionPacman = "";
+		else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+			this.direccionPacman = "";
+		else if (e.getKeyCode() == KeyEvent.VK_UP)
+			this.direccionPacman = "";
+		else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+			this.direccionPacman = "";
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		System.out.println("Coordenadas: " + e.getX() + ", " + e.getY());
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+
 	}
 
 }
