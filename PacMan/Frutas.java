d20bd2364e7fd7e9d72d107a1b97e31292d81293
AgusQuintanar <agusquintanar17@gmail.com>
2019-05-23 21:28:37 -0500

    Show score when pacman eats fruit

--- PacMan/Frutas.java
+++ PacMan/Frutas.java
@@ -1,4 +1,6 @@
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.image.ImageObserver;
 
 import javax.swing.ImageIcon;
 
@@ -11,15 +13,62 @@ public class Frutas {
 
     private int x,
             y,
-            anchoPista,
+            contadorInicial;
+
+    private double anchoPista,
             altoPista;
+
+    private boolean mostrarFruta,
+                    mostrarPuntaje;
     
-    public Frutas() {
+    public Frutas(double anchoPista, double altoPista) {
         this.imagenFruta = new ImageIcon("fruta1.png").getImage();
         this.arrayNivelesFruta = new int[]{1,2,3,3,4,4,5,5,6,6,7,7,8};
         this.arrayNivelesPuntuacionFruta = new int[]{100,300,500,500,700,700,1000,1000,2000,2000,3000,3000,5000};
-        this.x = (this.anchoPista / 2 - this.anchoPista / 104);
+        this.x = (int)(this.anchoPista / 2 - this.anchoPista / 104);
+        this.y = (int) ((17) * .9928 * (this.altoPista / 31) - .3 * .985 * (this.altoPista / 31) + .985 * this.altoPista / 62);
+        this.mostrarFruta = false;
     }
 
+    public int pintaFruta(Graphics g, int puntosComidos, int contador, int xPacMan, int yPacMan, int nivel, ImageObserver io) {
+        
+        int nivelTemp = 0;
+        if (nivel < 13) nivelTemp = nivel - 1;
+        else nivelTemp = 12;
+        
+        this.imagenFruta = new ImageIcon("fruta"+this.arrayNivelesFruta[nivelTemp]+".png").getImage();
+
+        if(!this.mostrarPuntaje) generarFruta(puntosComidos, contador);
+        int puntosAgregados = checarColisionConPacMan(xPacMan, yPacMan, contador, nivelTemp);
+
+        if(this.mostrarFruta) g.drawImage(this.imagenFruta, x, y, (int) (this.anchoPista/52), (int)(this.altoPista/31), io);
+        else if (this.mostrarPuntaje)  g.drawString(Integer.toString(this.arrayNivelesPuntuacionFruta[nivelTemp]), this.x, this.y + (int)(this.anchoPista/52/3));
+
+
+        return puntosAgregados;
+    }
+
+    public void generarFruta(int puntosComidos, int contador) {
+        if (puntosComidos == 70 || puntosComidos == 190){
+            this.contadorInicial = contador;
+            this.mostrarFruta = true;
+        }
+
+        if (contador - this.contadorInicial == 500) this.mostrarFruta = false;
+    }
+
+    public int checarColisionConPacMan(int xPacMan, int yPacMan, int contador, int nivel) {
+        if (Math.abs(this.x-xPacMan) < .3 && Math.abs(this.y-yPacMan) < .3){
+            this.mostrarFruta = false;
+            this.mostrarPuntaje = true;
+            this.contadorInicial = contador;
+        }
+
+        if (contador - this.contadorInicial == 200){
+            this.mostrarPuntaje = false;
+            return this.arrayNivelesPuntuacionFruta[nivel];
+        } 
+        return 0;
+    }
 
 }
